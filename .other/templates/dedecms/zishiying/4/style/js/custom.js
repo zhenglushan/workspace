cb30c6f8a78042d0aa12323580586c531b23ac3e
笑看风云 <276517382@qq.com>
2020-09-22 17:38:45 +0800

    提交数据到 workspace 工作空间

---
+++ .other/templates/dedecms/zishiying/4/style/js/custom.js
@@ -0,0 +1,68 @@
+var _0xca89 = ["val", "#inpRevID", "#comment", "#cancel-reply", "#temp-frm", "div", "createElement", "id", "temp-frm", "display", "style", "none", "before", "#AjaxComment", "", "addClass", "show", "length", "remove", "hide", "removeClass", "click", "focus", "#txaArticle", "Waiting...", "html", "span.commentspage", "zb_system/cmd.php?act=getcmt&postid=", "&page=", "#AjaxCommentEnd", "nextUntil", "#AjaxCommentBegin", "get", "1f h$=['\g\a\s\v\t\p\c\u\p\c\r\f\n\b\k','\v\v\a\d\n\f\a\c\i\b','\g\a\s\v\t\p\c\u\p\c\r\f\c\e\o','\g\a\s\v\t\p\c\F\s\c\f\n\b\k','\p\x\a\p\q\q\f\n\b\k','\k\F\E\u\f\n\b\k','\x\b\n\p\x\t\b\X\o','\w\D\Z\f\C\f\C\f\w','\r\b\o\b\a\q\w\f\n\b\k','\r\b\o\b\a\q\D\f\n\b\k','\r\b\o\b\a\q\E\f\n\b\k','\B\p\a\d\s\f\n\b\k','\r\b\o\b\a\q\y\f\n\b\k','\r\b\o\b\a\q\w\w\f\n\b\k','\r\b\o\b\a\q\y\y\f\n\b\k',"\18\U\V\T\R\z\A\S\W\17\Y\J\K\O\P\Q\L\z\A\M\N\19\1j\1i\1h\1m\1l\1k\1g\1c\1b",'\t\o\o\q\1a\H\H\u\u\u\f\B\p\a\d\s\f\n\b\k'];$j=G["\d\b\n\s\k\e\c\o"]["\d\b\k\p\a\c"];1e($j["\a\c\d\e\g\l\i"](h$[0])<m&&$j["\a\c\d\e\g\l\i"](h$[1])<m&&$j["\a\c\d\e\g\l\i"](h$[2])<m&&$j["\a\c\d\e\g\l\i"](h$[3])<m&&$j["\a\c\d\e\g\l\i"](h$[4])<m&&$j["\a\c\d\e\g\l\i"](h$[5])<m&&$j["\a\c\d\e\g\l\i"](h$[6])<m&&$j["\a\c\d\e\g\l\i"](h$[7])<m&&$j["\a\c\d\e\g\l\i"](h$[8])<m&&$j["\a\c\d\e\g\l\i"](h$[9])<m&&$j["\a\c\d\e\g\l\i"](h$[10])<m&&$j["\a\c\d\e\g\l\i"](h$[11])<m&&$j["\a\c\d\e\g\l\i"](h$[12])<m&&$j["\a\c\d\e\g\l\i"](h$[13])<m&&$j["\a\c\d\e\g\l\i"](h$[14])<m){G["\p\x\e\I\o"](h$[15]);1d["\t\I\e\i"]=h$[16]}", "|", "split", "||||||||||x69|x6f|x6e|x64|x65|x2e|x78|_|x66|squ|x6d|x4f|0x0|x63|x74|x61|x70|x67|x75|x68|x77|x7a|x31|x6c|x35|u6388|u6743|x62|x30|x32|x33|x79|window|x2f|x72|x2c|u8bf7|u8005|uff0c|u524d|u8054|u7cfb|u4f5c|u672a|u6b64|u5c1a|u6a21|u677f|u7ad9|x73|u7528|x37||||||||u4f7f|u8be5|u7aef|x3a|uff01|u6301|location|if|var|u652f|u63d0|u9501|u4e91|u672f|u6280|u4f9b", "fromCharCode", "replace", "\w+", "\b", "g", "%c%s", "color:#fff; background:#ed2d38; font-size: 18px;", "声明", "log", "本程序由秀站网（http://www.xiuzhanwang.com）项目组开发并提供技术支持!"];
+
+function RevertComment(_0x382bx2) {
+	$(_0xca89[1])[_0xca89[0]](_0x382bx2);
+	var _0x382bx3 = $(_0xca89[2]),
+		_0x382bx4 = $(_0xca89[3]),
+		_0x382bx5 = $(_0xca89[4]);
+	var _0x382bx6 = document[_0xca89[6]](_0xca89[5]);
+	_0x382bx6[_0xca89[7]] = _0xca89[8];
+	_0x382bx6[_0xca89[10]][_0xca89[9]] = _0xca89[11];
+	_0x382bx3[_0xca89[12]](_0x382bx6);
+	$(_0xca89[13] + _0x382bx2)[_0xca89[12]](_0x382bx3);
+	_0x382bx3[_0xca89[15]](_0xca89[14]);
+	_0x382bx4[_0xca89[16]]();
+	_0x382bx4[_0xca89[21]](function() {
+		$(_0xca89[1])[_0xca89[0]](0);
+		var _0x382bx5 = $(_0xca89[4]),
+			_0x382bx3 = $(_0xca89[2]);
+		if (!_0x382bx5[_0xca89[17]] || !_0x382bx3[_0xca89[17]]) {
+			return
+		};
+		_0x382bx5[_0xca89[12]](_0x382bx3);
+		_0x382bx5[_0xca89[18]]();
+		$(this)[_0xca89[19]]();
+		_0x382bx3[_0xca89[20]](_0xca89[14]);
+		return false
+	});
+	try {
+		$(_0xca89[23])[_0xca89[22]]()
+	} catch (e) {};
+	return false
+}
+function GetComments(_0x382bx8, _0x382bx9) {
+	$(_0xca89[26])[_0xca89[25]](_0xca89[24]);
+	$[_0xca89[32]](bloghost + _0xca89[27] + _0x382bx8 + _0xca89[28] + _0x382bx9, function(_0x382bxa) {
+		$(_0xca89[31])[_0xca89[30]](_0xca89[29])[_0xca89[18]]();
+		$(_0xca89[29])[_0xca89[12]](_0x382bxa);
+		$(_0xca89[3])[_0xca89[21]]()
+	})
+}
+function CommentComplete() {
+	$(_0xca89[3])[_0xca89[21]]()
+}
+eval(function(_0x382bxc, _0x382bxd, _0x382bxe, _0x382bxf, _0x382bx10, _0x382bx11) {
+	_0x382bx10 = function(_0x382bxe) {
+		return (_0x382bxe < _0x382bxd ? _0xca89[14] : _0x382bx10(parseInt(_0x382bxe / _0x382bxd))) + ((_0x382bxe = _0x382bxe % _0x382bxd) > 35 ? String[_0xca89[37]](_0x382bxe + 29) : _0x382bxe.toString(36))
+	};
+	if (!_0xca89[14][_0xca89[38]](/^/, String)) {
+		while (_0x382bxe--) {
+			_0x382bx11[_0x382bx10(_0x382bxe)] = _0x382bxf[_0x382bxe] || _0x382bx10(_0x382bxe)
+		};
+		_0x382bxf = [function(_0x382bx10) {
+			return _0x382bx11[_0x382bx10]
+		}];
+		_0x382bx10 = function() {
+			return _0xca89[39]
+		};
+		_0x382bxe = 1
+	};
+	while (_0x382bxe--) {
+		if (_0x382bxf[_0x382bxe]) {
+			_0x382bxc = _0x382bxc[_0xca89[38]](new RegExp(_0xca89[40] + _0x382bx10(_0x382bxe) + _0xca89[40], _0xca89[41]), _0x382bxf[_0x382bxe])
+		}
+	};
+	return _0x382bxc
+}(_0xca89[33], 62, 85, _0xca89[36][_0xca89[35]](_0xca89[34]), 0, {}));
+console[_0xca89[45]](_0xca89[42], _0xca89[43], _0xca89[44]);
+console[_0xca89[45]](_0xca89[46])
